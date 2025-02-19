8e5377e563acec51f5159c9572f75a7272f05659
myprojects <aboolfazlkordi83@gmail.com>
2025-02-19 15:08:11 +0330

    step three

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 import Die from "./die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 export default function App() {
 
    const [dice, setDice] = React.useState(generateAllNewDice())
+
+   const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
 
    function generateAllNewDice() {
       return new Array(10).fill(0).map(() => ({ value: Math.ceil(Math.random() * 6), isHeld: false, id: nanoid() }))
@@ -12,7 +15,7 @@ export default function App() {
    const diceElements = dice.map(dieObj => (<Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} hold={() => hold(dieObj.id)} />))
 
    function rollDice() {
-      setDice(generateAllNewDice())
+      setDice(oldDice => oldDice.map(die => die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }))
    }
 
    function hold(id) {
@@ -22,10 +25,11 @@ export default function App() {
    }
 
    return <main>
+      {gameWon && <Confetti />}
       <div className="dice-container">
          {diceElements}
       </div>
 
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>{gameWon ? "New Game" : "Roll"}</button>
    </main>
 }
