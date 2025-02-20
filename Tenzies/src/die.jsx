95f09ad9ca0d8ffc5e1d7ae3e078dac62d0ce19c
myprojects <aboolfazlkordi83@gmail.com>
2025-02-20 18:51:34 +0330

    End commit for this game

--- Tenzies/src/die.jsx
+++ Tenzies/src/die.jsx
@@ -1,8 +1,16 @@
-export default function die(props) {
+export default function Die(props) {
     const styles = {
-        backgroundcolor: props.isHeld ? "#59E391" : "white"
-    }
+        backgroundColor: props.isHeld ? "#59E391" : "white" 
+    };
+
     return (
-        <button style={styles} onClick={() => props.hold(props.id)} aria-pressed={props.isHeld} aria-label={`Die with value ${props.value},${props.isHeld ? "held" : "not held"}`} >{props.value}</button>
-    )
+        <button 
+            style={styles} 
+            onClick={() => props.hold(props.id)} 
+            aria-pressed={props.isHeld} 
+            aria-label={`Die with value ${props.value}, ${props.isHeld ? "held" : "not held"}`}
+        >
+            {props.value}
+        </button>
+    );
 }
